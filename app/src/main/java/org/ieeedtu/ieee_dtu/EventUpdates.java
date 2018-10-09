5a22591d492c99f9c6ce8b6368a57788ddbdaff6
shivamkumard107 <shivam.kumard107@gmail.com>
2018-10-09 07:23:04 +0530

    all necessary activities made and linked to MainActivity

---
+++ app/src/main/java/org/ieeedtu/ieee_dtu/EventUpdates.java
@@ -0,0 +1,15 @@
+package org.ieeedtu.ieee_dtu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class EventUpdates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_updates);
+    }
+}
